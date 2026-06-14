582fe99c8d0ebd80bfee09a3f120bd3f9773cd08
Delph1 <andreas.galiste@gmail.com>
2026-06-14 15:24:09 +0200

    Interface added

--- Laktaren.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Laktaren.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Laktaren.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Laktaren.Domain.Entities;
+
+namespace Laktaren.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task AddAsync(User user);
+        Task SaveChangesAsync();
+    }
+}
